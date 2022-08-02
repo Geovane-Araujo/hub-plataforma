cf3f9d6b852995705d67f8e23f604bf58e5cdd5b
Geovane Araújo <geovane-araujo@outlook.com>
2022-08-01 23:37:53 -0300

    Starter

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    Home
+    <button @click="onClickRoute">Clique em min</button>
+    <iframe id="ci"></iframe>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  setup () {
+    const onClickRoute = () => {
+      document.getElementById('ci').src = 'https://hub.adonaisoft.com.br/oficinaplus/'
+    }
+    return { onClickRoute }
+  },
+  components: {
+  }
+}
+</script>
+<style>
+iframe{
+  height: 500px;
+  width: 500px;
+  margin: 20px;
+}
+</style>
